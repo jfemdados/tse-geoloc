119971e2ce59142cef99a14cb6a1ab8b24f88684
baarthur <baarthur0@outlook.com>
2024-10-08 15:19:12 -0300

    agora com dados de 2022 e 2024 e pronto para outros

--- src/geoloc.R
+++ src/geoloc.R
@@ -10,33 +10,57 @@
 
 library(dplyr)
 library(readr)
+library(purrr)
 
 
 
 # download ------------------------------------------------------------------------------------
 
-url_tse <- paste0("https://cdn.tse.jus.br/estatistica/sead/odsele/",
-                  "eleitorado_locais_votacao/eleitorado_local_votacao_2024.zip")
+anos <- c(2022, 2024)
 
-download.file(url_tse, "data-raw/db_eleitorado_2024.zip")
+url_tse <- anos %>% 
+  map(
+    \(x) paste0("https://cdn.tse.jus.br/estatistica/sead/odsele/",
+                "eleitorado_locais_votacao/eleitorado_local_votacao_", x, ".zip")
+  ) %>% 
+  set_names(anos)
 
-archive::archive_extract("data-raw/db_eleitorado_2024.zip", "data-raw")
+anos %>% 
+  map(
+    \(x) download.file(url_tse[[as.character(x)]], paste0("data-raw/db_eleitorado_", x, ".zip"))
+  )
+
+list.files("data-raw", pattern = "db_eleitorado", full.names = T) %>% 
+  map(archive::archive_extract, dir = "data-raw")
+
+archive::archive_extract("data-raw/db_eleitorado_2022.zip", "data-raw")
 
 
 
 # geolocalizar locais de voto -----------------------------------------------------------------
 
-db_tse <- read_csv2("data/eleitorado_local_votacao_2024.csv", locale = locale(encoding = "latin1"))
+db_tse <- list.files("data-raw", pattern = "\\.csv$", full.names = T) %>%
+  map(read_csv2, locale = locale(encoding = "latin1")) %>% 
+  set_names(anos)
 
 db_tse <- db_tse %>% 
-  filter(NM_MUNICIPIO %in% c("BELO HORIZONTE", "SANTOS DUMONT", "JUIZ DE FORA"))
+  map(\(x) x %>% filter(NM_MUNICIPIO %in% c("BELO HORIZONTE", "SANTOS DUMONT", "JUIZ DE FORA")))
 
+
+## aqui, criamos uma lista de endereços independentemente do ano. 
 db_locais <- db_tse %>% 
+  bind_rows() %>% 
   distinct(NM_LOCAL_VOTACAO, DS_ENDERECO, NR_CEP, NM_MUNICIPIO) %>% 
   mutate(addr = paste0(DS_ENDERECO, " - ", NM_MUNICIPIO, ", ", NR_CEP))
 
+
+## para ATUALIZAR a lista, pule o restante da seção e vá para a próxima.
+
+
+## geolocalizar
 db_locais <- db_locais %>% 
   tidygeocoder::geocode(address = addr, method = "here")
+
 
 ## consertar na mão escola mal geolocalizada em Santos Dumont
 db_locais <- db_locais %>% 
@@ -56,15 +80,42 @@ db_locais %>%
 
 # db_locais <- readRDS("data/shp/locais_voto.RDS")
 
+
+
+# opcional: atualizar a lista -----------------------------------------------------------------
+
+## para incluir mais anos no futuro ou atualizar a lista, faça o seguinte:
+
+### carregue os dados originais
+db_locais_0 <- readRDS("data/shp/locais_voto.RDS")
+
+### faça um anti_join para manter apenas os locais ausentes
+db_locais <- anti_join(db_locais, db_locais_0)
+
+### geolocalize o restante
+db_locais <- db_locais %>% 
+  tidygeocoder::geocode(address = addr, method = "here")
+
+### junte tudo
+db_locais <- bind_rows(db_locais_0, db_locais)
+
+
+### e salve!
+db_locais %>% 
+  saveRDS("data/shp/locais_voto.RDS")
+
  
  
 # jogar a info de volta nas seções eleitorais -------------------------------------------------
 
 db_tse <- db_tse %>% 
-  select(AA_ELEICAO, DT_ELEICAO, DS_ELEICAO, NR_TURNO, NM_MUNICIPIO, NR_ZONA, NR_SECAO,
-         NM_LOCAL_VOTACAO, DS_ENDERECO, NR_CEP) %>% 
-  left_join(db_locais) %>% 
-  select(-addr)
+  map(
+    \(x) x %>% 
+      select(AA_ELEICAO, DT_ELEICAO, DS_ELEICAO, NR_TURNO, NM_MUNICIPIO, NR_ZONA, NR_SECAO,
+             NM_LOCAL_VOTACAO, DS_ENDERECO, NR_CEP) %>% 
+      left_join(db_locais) %>% 
+      select(-addr)
+  )
 
 db_tse %>% 
   saveRDS("data/shp/secoes_geolocalizadas.RDS")
